da8c54afcac3aee33128574d158a1064de563bc4
yura <yura_pykish1994@mail.ru>
2017-01-06 14:32:00 +0200

    add students

---
+++ students/views/students.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def students_list(request):
+    students = (
+        {'id': 1,
+         'first_name': u'Віталій',
+         'last_name': u'Подоба',
+         'ticket': 235,
+         'image': 'img/me.jpeg'},
+        {'id': 2,
+         'first_name': u'Корост',
+         'last_name': u'Андрій',
+         'ticket': 2123,
+         'image': 'img/piv.png'},
+    )
+    return render(request, 'students/students_list.html',
+        {'students': students})
+
+def students_add(request):
+    return HttpResponse('<h1>Student Add Form</h1>')
+
+def students_edit(request, sid):
+    return HttpResponse('<h1>Edit Student %s</h1>' % sid)
+
+def students_delete(request, sid):
+    return HttpResponse('<h1>Delete Student %s</h1>' % sid)
--- studentsdb/urls.py
+++ studentsdb/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-]
+urlpatterns = patterns(’’,
+# Students urls
+url(r’^$’, ’students.views.students_list’, name=’home’),
+url(r’^admin/’, include(admin.site.urls)),
+)
